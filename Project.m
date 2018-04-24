fcc31e3eb96471813913b8d7c7c19bfb3931455f
btrosse1 <38545129+btrosse1@users.noreply.github.com>
2018-04-24 10:35:36 -0400

    Add files via upload

--- Project.m
+++ Project.m
@@ -22,7 +22,7 @@ function varargout = Project(varargin)
 
 % Edit the above text to modify the response to help Project
 
-% Last Modified by GUIDE v2.5 21-Apr-2018 11:23:49
+% Last Modified by GUIDE v2.5 24-Apr-2018 09:58:30
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -259,4 +259,17 @@ function KValEText_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+end
+
+
+% --- Executes on button press in videoButton.
+function videoButton_Callback(hObject, eventdata, handles)
+% hObject    handle to videoButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+vid= VideoReader('VideoforGUI.mp4');
+mov= read(vid);
+implay(mov);
+
 end
